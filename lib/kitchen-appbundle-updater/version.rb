beb4a10e10124e0c75b4475a897642d24b4e19c3
Jay Mundrawala <jdmundrawala@gmail.com>
2016-01-19 09:49:27 -0800

    Deprecate this repo

--- lib/kitchen-appbundle-updater/version.rb
+++
@@ -1,3 +0,0 @@
-class KitchenAppbundleUpdater
-  VERSION = '0.0.4'
-end
--- support/chef_base_updater.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop";
-
-Function Log($m) { Write-Host "       $m`n" }
-
-Log "Finding cookbook to update chef"
-$env:Path = $env:Path + ";$chef_omnibus_root\bin\"
-Log "Running chef-client with recipe chef-appbundle-updater::default from $cookbook_url"
-& "$chef_omnibus_root\bin\chef-client.bat" -z --recipe-url "$cookbook_url" -j "$json" -o "recipe[chef-appbundle-updater::default]"
